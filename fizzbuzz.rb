0a3bded419b069028cc9b4172e5fbf683e140966
Derek Townsend <derekjavon2@gmail.com>
2019-04-27 17:03:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,10 @@ def fizzbuzz(num)
   output=nil
   if (((num%3)==0)&&((num%5)==0))
     output="FizzBuzz"
-  if ((num%3)==0)
+  elsif ((num%3)==0)
     output="Fizz"
   elsif ((num%5)==0)
     output="Buzz"
   end
-
   return output
 end
